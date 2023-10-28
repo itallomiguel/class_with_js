daa30c4724906a913a5f9d0411b34351bd331ab3
Itallo Miguel <120153332+itallomiguel@users.noreply.github.com>
2023-10-28 03:22:13 -0300

    Update index.js
    added a new client in the system

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const Cliete = require("./conta");
 const cliente1 = new Cliete("Ronald", 31, "ronalddeivid@gmail.com");
 
 console.log(cliente1.retorna_nome);
+
+console.log(cliente1.retorna_email):
+
+const cliente2 = new Cliente("Quitinhozinhu", 1, "quitinreidaspriquita@gmail.com"
+                             console.log(cliente2.retorna_nome);
